f5251b78a7989926c00ba3bb464d332e3010d12d
oscar luna <os.lun11@gmail.com>
2017-06-13 07:24:43 -0700

    gave app an uplift

--- server.js
+++ server.js
@@ -1,28 +1,30 @@
 var express = require("express");
-var methodOverride = require("method-override");
 var bodyParser = require("body-parser");
+var methodOverride = require("method-override");
 
-var port = 3000;
+var PORT = process.env.PORT || 3000;
 
 var app = express();
 
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static(process.cwd() + "/public"));
 
-app.use(bodyParser.urlencoded({ extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 
-//overide with Post having ?_method=DELETE
+// Override with POST having ?_method=PUT
 app.use(methodOverride("_method"));
 
-//Set handlers
+// Set Handlebars.
 var exphbs = require("express-handlebars");
 
-app.engine("handlebars", exphbs({ defaultLayout: "main.handlebars"}));
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-//Import routes and give the server access to them.
-var routes = require("./controllers/burger_controller.js");
+// Import routes and give the server access to them.
+var routes = require("./controllers/burgers_controller.js");
 
 app.use("/", routes);
 
-app.listen(port);
+app.listen(PORT, function() {
+    console.log("You are connected on port: " + PORT)
+});
