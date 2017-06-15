c173b05034502ca21940b64cded25134f318463f
oscar luna <os.lun11@gmail.com>
2017-06-14 19:27:36 -0700

    SETUP

--- config/connection.js
+++ config/connection.js
@@ -5,8 +5,7 @@ var connection = mysql.createConnection({
   host: "us-cdbr-iron-east-03.cleardb.net",
   user: "b2db902830d847",
   password: "c4e95f70",
-  database: "burger_db",
-  // database: "heroku_371d1f1f73c9037",
+  database: "371d1f1f73c9037",
   connectionLimit: 5 
 });
 
